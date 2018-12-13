62dc8f152dbe152c919dc7a74e1098a811958b45
Nick Spreitzer <nick@refactorsaurusrex.com>
2018-12-13 17:12:55 -0600

    Add cmdlet to open backup location

---
+++ src/JournalCli/Commands/OpenBackupLocationCmdlet.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Management.Automation;
+using JetBrains.Annotations;
+
+namespace JournalCli.Commands
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Open, "BackupLocation")]
+    public class OpenBackupLocationCmdlet : CmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            var path = UserSettings.Load().BackupLocation;
+            if (!string.IsNullOrEmpty(path) && Directory.Exists(path))
+            {
+                Process.Start(new ProcessStartInfo(path)
+                {
+                    UseShellExecute = true
+                });
+            }
+            else
+            {
+                ThrowTerminatingError("Backup location not found", ErrorCategory.InvalidOperation);
+            }
+        }
+    }
+}
