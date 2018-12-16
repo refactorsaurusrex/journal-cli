0e153ce7154fcfb391f8ccdb823397182645c003
Nick Spreitzer <nick@refactorsaurusrex.com>
2018-12-15 23:05:05 -0600

    Rename cmdlet to use more appropriate verb

---
+++ src/JournalCli/Commands/OpenRandomJournalEntryCmdlet.cs
@@ -0,0 +1,23 @@
+﻿using System.Management.Automation;
+using JetBrains.Annotations;
+
+namespace JournalCli.Commands
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Open, "RandomJournalEntry")]
+    public class OpenRandomJournalEntryCmdlet : JournalCmdletBase
+    {
+        [Parameter]
+        public string[] Tags { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var root = GetResolvedRootDirectory();
+
+            if (Tags.Length == 0)
+                Journal.OpenRandomEntry(root);
+            else
+                Journal.OpenRandomEntry(root, Tags);
+        }
+    }
+}
