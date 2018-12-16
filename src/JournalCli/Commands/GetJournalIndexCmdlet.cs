e651235b389a260863f1fe5c00c502bb30f013db
Nick Spreitzer <nick@refactorsaurusrex.com>
2018-12-15 23:02:26 -0600

    wip: Add cmdlet documentation

--- src/JournalCli/Commands/GetJournalIndexCmdlet.cs
+++ src/JournalCli/Commands/GetJournalIndexCmdlet.cs
@@ -4,6 +4,11 @@ using System.Linq;
 
 namespace JournalCli.Commands
 {
+    /// <summary>
+    /// <para type="synopsis">This is the cmdlet synopsis.</para>
+    /// <para type="description">This is part of the longer cmdlet description.</para>
+    /// <para type="description">This is also part of the longer cmdlet description.</para>
+    /// </summary>
     [PublicAPI]
     [Cmdlet(VerbsCommon.Get, "JournalIndex")]
     [OutputType(typeof(JournalIndex))]
