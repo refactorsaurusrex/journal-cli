49fad2d18e2a9e860bc066ccabc246064d75c2b4
Nick Spreitzer <nick@refactorsaurusrex.com>
2018-12-15 23:06:08 -0600

    Allow opening random entries, filtered by tag

--- src/JournalCli/Journal.cs
+++ src/JournalCli/Journal.cs
@@ -2,18 +2,37 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Management.Automation;
 
 namespace JournalCli
 {
     public class Journal
     {
-        public static void OpenRandomJournalEntry(string rootDirectory)
+        public static void OpenRandomEntry(string rootDirectory, string[] tags)
+        {
+            var journalIndex = CreateIndex(rootDirectory, false);
+            if (journalIndex.Count == 0)
+                throw new PSInvalidOperationException("I couldn't find any journal entries. Did you pass in the right root directory?");
+
+            var allTaggedEntries = journalIndex.Where(x => tags.Contains(x.Tag)).ToList();
+            var random = new Random();
+            var randomIndex1 = random.Next(0, allTaggedEntries.Count - 1);
+            var entries = allTaggedEntries[randomIndex1].Entries;
+            var randomIndex2 = random.Next(0, entries.Count - 1);
+
+            Process.Start(new ProcessStartInfo(entries.ElementAt(randomIndex2).FilePath)
+            {
+                UseShellExecute = true
+            });
+        }
+
+        public static void OpenRandomEntry(string rootDirectory)
         {
             var di = new DirectoryInfo(rootDirectory);
-            var entries = di.GetFiles("*.md", SearchOption.AllDirectories).Where(x => x.LastWriteTime < DateTime.Now.AddMonths(-1)).ToList();
+            var entries = di.GetFiles("*.md", SearchOption.AllDirectories).ToList();
 
             if (entries.Count == 0)
-                throw new InvalidOperationException("I couldn't find any entries older than a month. Did you pass in the right root directory?");
+                throw new PSInvalidOperationException("I couldn't find any journal entries. Did you pass in the right root directory?");
 
             var index = new Random().Next(0, entries.Count - 1);
             Process.Start(new ProcessStartInfo(entries[index].FullName)
