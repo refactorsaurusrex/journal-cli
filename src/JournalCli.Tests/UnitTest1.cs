d59d959bfed025dfce17203cdfda6e9f15714554
Nick Spreitzer <nick@200-ok.com>
2019-08-11 20:01:33 -0500

    Add unit tests project

---
+++ src/JournalCli.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace JournalCli.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var md = new MarkdownFiles();
+            //var files = md.FindAll("J:\\");
+        }
+    }
+}
