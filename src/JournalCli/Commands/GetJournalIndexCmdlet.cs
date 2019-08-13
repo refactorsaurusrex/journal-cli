231a26af54e2b34e65ed70cc91c7b5c2199f2173
Nick Spreitzer <nick@200-ok.com>
2019-08-12 23:10:32 -0500

    Add markdown based docs

--- src/JournalCli/Commands/GetJournalIndexCmdlet.cs
+++ src/JournalCli/Commands/GetJournalIndexCmdlet.cs
@@ -4,11 +4,6 @@ using System.Linq;
 
 namespace JournalCli.Commands
 {
-    /// <summary>
-    /// <para type="synopsis">This is the cmdlet synopsis.</para>
-    /// <para type="description">This is part of the longer cmdlet description.</para>
-    /// <para type="description">This is also part of the longer cmdlet description.</para>
-    /// </summary>
     [PublicAPI]
     [Cmdlet(VerbsCommon.Get, "JournalIndex")]
     [OutputType(typeof(JournalIndex))]
