0094270191eb007acc7a9f1db924532685c8c078
Nick Spreitzer <nick@200-ok.com>
2019-08-28 22:50:50 -0500

    Add Windows encrypted store tests

--- src/JournalCli.Tests/WindowsEncryptedStoreTests.cs
+++ src/JournalCli.Tests/WindowsEncryptedStoreTests.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using FakeItEasy;
+using FluentAssertions;
 using Xunit;
 
 namespace JournalCli.Tests
@@ -6,10 +10,62 @@ namespace JournalCli.Tests
     public class WindowsEncryptedStoreTests
     {
         [Fact]
-        public void Test1()
+        public void Save_WritesCipherAndEntropyToDisk()
         {
-            //var md = new MarkdownFiles();
-            //var files = md.FindAll("J:\\");
+            var fileSystem = A.Fake<IFileSystem>();
+            var store = new WindowsEncryptedStore<UserSettings>(fileSystem);
+            var settings = new UserSettings { BackupPassword = "secret" };
+            store.Save(settings);
+
+            A.CallTo(() => fileSystem.File.WriteAllBytes(A<string>.Ignored, A<byte[]>.Ignored)).MustHaveHappenedTwiceExactly();
+        }
+
+        [Fact]
+        public void Load_CorrectlyRestoresSavedValue()
+        {
+            var fileSystem = new MockFileSystem();
+            var store = new WindowsEncryptedStore<UserSettings>(fileSystem);
+            var settings = new UserSettings
+            {
+                BackupPassword = "secret",
+                BackupLocation = "C:\\MyJournalBackup",
+                DefaultJournalRoot = "D:\\MyJournal"
+            };
+            store.Save(settings);
+
+            var result = store.Load();
+            result.Should().BeEquivalentTo(settings);
+        }
+
+        [Fact]
+        public void WindowsEncryptedStore_CreatesDataDirectory_WhenItDoesntExist()
+        {
+            var fileSystem = new MockFileSystem();
+            fileSystem.AddDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            var dataDirectory = fileSystem.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "JournalCli");
+
+            fileSystem.AllDirectories.Should().NotContain(dataDirectory);
+
+            var store = new WindowsEncryptedStore<UserSettings>(fileSystem);
+            var settings = new UserSettings
+            {
+                BackupPassword = "secret",
+                BackupLocation = "C:\\MyJournalBackup",
+                DefaultJournalRoot = "D:\\MyJournal"
+            };
+            store.Save(settings);
+
+            fileSystem.AllDirectories.Should().Contain(dataDirectory);
+        }
+
+        [Fact]
+        public void Load_ReturnsEmptyObject_WhenNoSavedObjectExists()
+        {
+            var fileSystem = new MockFileSystem();
+            var store = new WindowsEncryptedStore<UserSettings>(fileSystem);
+
+            var result = store.Load();
+            result.Should().BeEquivalentTo(new UserSettings());
         }
     }
 }
