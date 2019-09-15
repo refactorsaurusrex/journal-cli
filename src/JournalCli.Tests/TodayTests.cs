5e47e8af6e280c665f97806509d0cf5c6d464768
Nick Spreitzer <nick@200-ok.com>
2019-09-14 22:29:52 -0500

    Add 'Today' type

---
+++ src/JournalCli.Tests/TodayTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using JournalCli.Infrastructure;
+using NodaTime;
+using Xunit;
+
+namespace JournalCli.Tests
+{
+    public class TodayTests
+    {
+        [Fact]
+        public void MinusDays_ReturnsExpectedDate()
+        {
+            var fixture = new Fixture();
+            var count = fixture.Create<int>();
+            var result = Today.MinusDays(count);
+            var expected = LocalDate.FromDateTime(DateTime.Now.AddDays(count * -1).Date);
+
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void MinusMonths_ReturnsExpectedDate()
+        {
+            var fixture = new Fixture();
+            var count = fixture.Create<int>();
+            var result = Today.MinusMonths(count);
+            var expected = LocalDate.FromDateTime(DateTime.Now.AddMonths(count * -1).Date);
+
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void MinusYears_ReturnsExpectedDate()
+        {
+            var fixture = new Fixture();
+            var count = fixture.Create<int>();
+            var result = Today.MinusYears(count);
+            var expected = LocalDate.FromDateTime(DateTime.Now.AddYears(count * -1).Date);
+
+            result.Should().Be(expected);
+        }
+    }
+}
