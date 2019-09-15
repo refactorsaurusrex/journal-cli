9717c9de022be9744e1b3af7f64377da1f050abf
Nick Spreitzer <nick@200-ok.com>
2019-09-15 10:09:47 -0500

    Add journal entry subtype that includes Headers and Body

---
+++ src/JournalCli/Core/CompleteJournalEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using JournalCli.Infrastructure;
+
+namespace JournalCli.Core
+{
+    public class CompleteJournalEntry : JournalEntry
+    {
+        public CompleteJournalEntry(IJournalReader journalReader)
+            : base(journalReader)
+        {
+            Headers = journalReader.Headers;
+            Body = journalReader.Body;
+        }
+
+        public ICollection<string> Headers { get; }
+
+        public string Body { get; }
+    }
+}
