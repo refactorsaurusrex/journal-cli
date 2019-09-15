d7a34b8f674643a60c86ddce77cb763980ae1e2a
Nick Spreitzer <nick@200-ok.com>
2019-09-15 10:09:08 -0500

    Add wrapper around ShouldContinue
    This is for simplicity and consistency.

--- src/JournalCli/Cmdlets/CmdletBase.cs
+++ src/JournalCli/Cmdlets/CmdletBase.cs
@@ -13,6 +13,16 @@ namespace JournalCli.Cmdlets
             ThrowTerminatingError(errorRecord);
         }
 
+        /// <summary>
+        /// Prompts the user to confirm their actions before proceeding. 
+        /// </summary>
+        /// <param name="warning">A statement (not a question) about what is about to happen.</param>
+        /// <returns>True if the user wants to proceed. False if they want to abort.</returns>
+        protected bool AreYouSure(string warning)
+        {
+            return ShouldContinue("Do you want to continue?", warning);
+        }
+
         protected void WriteHost(string text, ConsoleColor foregroundColor = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black)
         {
             Host.UI.WriteLine(foregroundColor, backgroundColor, text);
