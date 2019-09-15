c10cffe2330aaa20670c9d9bb853e86e917b5205
Nick Spreitzer <nick@200-ok.com>
2019-09-14 22:33:13 -0500

    Add journal readme value parser

---
+++ src/JournalCli.Tests/ReadmeParserTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using JournalCli.Infrastructure;
+using NodaTime;
+using Xunit;
+
+namespace JournalCli.Tests
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class ReadmeParserTests
+    {
+        [Theory]
+        [InlineData("9/7/2019", "9-7-2019")]
+        [InlineData("9.7.2019", "9-7-2019")]
+        [InlineData("09/07/2019", "9-7-2019")]
+        [InlineData("9/07/2019", "9-7-2019")]
+        [InlineData("09/7/2019", "9-7-2019")]
+        [InlineData("09\\7\\2019", "9-7-2019")]
+        [InlineData("9-7-19", "9-7-2019")]
+        [InlineData("09-07-19", "9-7-2019")]
+        [InlineData("09.7.19", "9-7-2019")]
+        [InlineData("1 year", "4-25-2020")]
+        [InlineData("6 months", "10-25-2019")]
+        [InlineData("56 days", "6-20-2019")]
+        [InlineData("27 week", "10-31-2019")]
+        [InlineData("27 weeks", "10-31-2019")]
+        [InlineData("1 week", "5-2-2019")]
+        public void This_CanParse_AllValidDateFormats(string readmeValue, string expectedResult)
+        {
+            // TODO: Eventually, perhaps, this will need to be adjust to allow for non-American formats. 
+            var journalDate = LocalDate.FromDateTime(DateTime.Parse("4-25-2019"));
+            var parser = new ReadmeParser(readmeValue, journalDate);
+            parser.FormattedExpirationDate.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Theory]
+        [InlineData("not valid")]
+        [InlineData("notvalid")]
+        [InlineData("123")]
+        [InlineData("asdf")]
+        [InlineData("this is a test")]
+        [InlineData("!@#$%^")]
+        public void This_ThrowsException_WhenReadmeIsInvalid(string invalidReadme)
+        {
+            var journalDate = LocalDate.FromDateTime(DateTime.Parse("4-25-2019"));
+            Assert.Throws<FormatException>(() => new ReadmeParser(invalidReadme, journalDate));
+        }
+    }
+}
---
+++ src/JournalCli/Infrastructure/ReadmeParser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using NodaTime;
+
+namespace JournalCli.Infrastructure
+{
+    internal class ReadmeParser
+    {
+        public ReadmeParser(string readmeText, LocalDate journalDate)
+        {
+            if (readmeText.Contains(" "))
+            {
+                var readmeArray = readmeText.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var duration = int.Parse(readmeArray[0]);
+
+                LocalDate expiration;
+                string period;
+
+                if (Regex.IsMatch(readmeArray[1], @"days?"))
+                {
+                    expiration = journalDate.PlusDays(duration);
+                    period = duration == 1 ? "day" : "days";
+                }
+                else if (Regex.IsMatch(readmeArray[1], @"weeks?"))
+                {
+                    expiration = journalDate.PlusWeeks(duration);
+                    period = duration == 1 ? "week" : "weeks";
+                }
+                else if (Regex.IsMatch(readmeArray[1], @"months?"))
+                {
+                    expiration = journalDate.PlusMonths(duration);
+                    period = duration == 1 ? "month" : "months";
+                }
+                else if (Regex.IsMatch(readmeArray[1], @"years?"))
+                {
+                    expiration = journalDate.PlusYears(duration);
+                    period = duration == 1 ? "year" : "years";
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+
+                ExpirationDate = expiration;
+                FormattedExpirationDate = expiration.ToString("M-d-yyyy", CultureInfo.CurrentCulture);
+                FrontMatterValue = $"{duration} {period}";
+            }
+            else
+            {
+                readmeText = readmeText.Replace('\\', '/'); // Just in case these were used.
+                var dt = DateTime.Parse(readmeText);
+                ExpirationDate = LocalDate.FromDateTime(dt);
+                FrontMatterValue = FormattedExpirationDate = dt.ToString("M-d-yyyy", CultureInfo.CurrentCulture);
+            }
+        }
+
+        // TODO: Test that ExpirationDate is correct value
+        public LocalDate ExpirationDate { get; }
+
+        public string FormattedExpirationDate { get; }
+
+        public string FrontMatterValue { get; }
+    }
+}
