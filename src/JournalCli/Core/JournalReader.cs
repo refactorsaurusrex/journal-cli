43ce93b110c99109004692a31fe9b975ad306e27
Nick Spreitzer <nick@200-ok.com>
2019-09-14 23:11:48 -0500

    Encapsulate logic for reading journal files from disk

---
+++ src/JournalCli/Core/JournalReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using JournalCli.Infrastructure;
+using NodaTime;
+using NodaTime.Text;
+
+namespace JournalCli.Core
+{
+    internal class JournalReader : IJournalReader
+    {
+        public JournalReader(IFileSystem fileSystem, string filePath)
+        {
+            FilePath = filePath;
+            var lines = fileSystem.File.ReadAllLines(FilePath).ToList();
+            Headers = lines.Where(x => x.StartsWith("#")).ToList();
+            var bodyStartIndex = lines.FindLastIndex(s => s == JournalFrontMatter.BlockIndicator) + 1;
+            Body = string.Join(Environment.NewLine, lines.Skip(bodyStartIndex));
+            FrontMatter = JournalFrontMatter.FromFilePath(fileSystem, filePath);
+            EntryName = fileSystem.Path.GetFileNameWithoutExtension(FilePath) ?? throw new InvalidOperationException();
+            EntryDate = LocalDatePattern.CreateWithCurrentCulture("yyyy.MM.dd").Parse(EntryName).Value;
+        }
+
+        public string Body { get; }
+
+        public IJournalFrontMatter FrontMatter { get; }
+
+        public ICollection<string> Headers { get; }
+
+        public string FilePath { get; }
+
+        public string EntryName { get; }
+
+        public LocalDate EntryDate { get; }
+    }
+}
---
+++ src/JournalCli/Core/JournalReaderFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Abstractions;
+using JournalCli.Infrastructure;
+
+namespace JournalCli.Core
+{
+    internal class JournalReaderFactory : IJournalReaderFactory
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public JournalReaderFactory(IFileSystem fileSystem) => _fileSystem = fileSystem;
+
+        public JournalReader CreateReader(string filePath) => new JournalReader(_fileSystem, filePath);
+    }
+}
