908e6a7b491010d9c8977a37361937f12fdf6ec9
Nick Spreitzer <nick@200-ok.com>
2019-09-29 16:23:29 -0500

    Enable opening entries by name or date

---
+++ src/JournalCli/Cmdlets/OpenJournalEntryCmdlet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using System.IO.Abstractions;
+using System.Management.Automation;
+using JetBrains.Annotations;
+using JournalCli.Core;
+using JournalCli.Infrastructure;
+using NodaTime;
+
+namespace JournalCli.Cmdlets
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Open, "JournalEntry")]
+    public class OpenJournalEntryCmdlet : JournalCmdletBase
+    {
+        // TODO: open entry via date or natural language input
+
+        [Parameter(ValueFromPipeline = true, Position = 0, ParameterSetName = "Name")]
+        public string EntryName { get; set; }
+
+        [Parameter(ValueFromPipeline = true, Position = 0, ParameterSetName = "Date")]
+        public DateTime Date { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            var fileSystem = new FileSystem();
+            string path;
+
+            if (ParameterSetName == "Name")
+            {
+                if (!EntryName.EndsWith(".md"))
+                    EntryName += ".md";
+                var date = Journal.FileNameWithExtensionPattern.Parse(EntryName).Value;
+                var year = date.ToString(Journal.YearDirectoryPattern.PatternText, CultureInfo.CurrentCulture);
+                var month = date.ToString(Journal.MonthDirectoryPattern.PatternText, CultureInfo.CurrentCulture);
+                path = fileSystem.Path.Combine(RootDirectory, year, month, EntryName);
+            }
+            else if (ParameterSetName == "Date")
+            {
+                var date = LocalDate.FromDateTime(Date);
+                var year = date.ToString(Journal.YearDirectoryPattern.PatternText, CultureInfo.CurrentCulture);
+                var month = date.ToString(Journal.MonthDirectoryPattern.PatternText, CultureInfo.CurrentCulture);
+                var name = LocalDate.FromDateTime(Date).ToJournalEntryFileName();
+                path = fileSystem.Path.Combine(RootDirectory, year, month, name);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+
+            if (!fileSystem.File.Exists(path))
+                throw new PSInvalidOperationException("That journal entry doesn't currently exist.");
+
+            var systemProcess = new SystemProcess();
+            systemProcess.Start(path);
+        }
+    }
+}
