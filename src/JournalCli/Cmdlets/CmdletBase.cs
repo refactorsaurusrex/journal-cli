9696fe2685599fa0a8ed05228d1c7642c6864611
Nick Spreitzer <nick@200-ok.com>
2019-09-29 16:10:22 -0500

    Refactor rename tag operations
    Changed the DryRun output, updated the AreYouSure wrapper method, and added a method to write a header block to the host.

--- src/JournalCli/Cmdlets/CmdletBase.cs
+++ src/JournalCli/Cmdlets/CmdletBase.cs
@@ -19,9 +19,16 @@ namespace JournalCli.Cmdlets
         /// </summary>
         /// <param name="warning">A statement (not a question) about what is about to happen.</param>
         /// <returns>True if the user wants to proceed. False if they want to abort.</returns>
-        protected bool AreYouSure(string warning)
+        protected bool AreYouSure(string warning) => ShouldContinue("Are you certain you want to continue?", "ACTION: " + warning);
+
+        protected void WriteHeader(string title, ConsoleColor color)
         {
-            return ShouldContinue("Do you want to continue?", warning);
+            var windowWidth = Host.UI.RawUI.WindowSize.Width;
+            var headerWidth = Math.Min(75, windowWidth);
+
+            WriteHost(new string('=', headerWidth), color);
+            WriteHost(title, color);
+            WriteHost(new string('=', headerWidth), color);
         }
 
         protected void WriteHost(string text, ConsoleColor foregroundColor = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black)
