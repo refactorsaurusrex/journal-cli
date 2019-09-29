c34cb22753d75dbedd9dcfed4d7403a00ccfce4b
Nick Spreitzer <nick@200-ok.com>
2019-09-29 16:13:06 -0500

    Add cmdlet to manually create commits

---
+++ src/JournalCli/Cmdlets/SaveJournalSnapshotCmdlet.cs
@@ -0,0 +1,20 @@
+﻿using System.Management.Automation;
+using JetBrains.Annotations;
+
+namespace JournalCli.Cmdlets
+{
+    [PublicAPI]
+    [Cmdlet(VerbsData.Save, "JournalSnapshot")]
+    public class SaveJournalSnapshotCmdlet : JournalCmdletBase
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        [ValidateLength(5, 60)]
+        public string Message { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            Commit(Message);
+        }
+    }
+}
