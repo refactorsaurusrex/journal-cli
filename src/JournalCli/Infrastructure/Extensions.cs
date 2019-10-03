9cf974dcc3bcabce41efc0509256b4cd943e7343
Nick Spreitzer <nick@200-ok.com>
2019-10-03 12:45:06 -0500

    Make parsing journal entry file names easier

---
+++ src/JournalCli/Infrastructure/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using JournalCli.Core;
+using NodaTime;
+
+namespace JournalCli.Infrastructure
+{
+    internal static class Extensions
+    {
+        public static bool ContainsAll<T>(this IEnumerable<T> target, IEnumerable<T> matches) => matches.All(target.Contains);
+
+        public static string ToJournalEntryFileName(this LocalDate date) => date.ToString(Journal.FileNameWithExtensionPattern.PatternText, CultureInfo.CurrentCulture);
+    }
+}
