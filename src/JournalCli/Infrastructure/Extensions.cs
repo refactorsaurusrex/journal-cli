84ea810ac081b83e33bdf63b9b76e209456e899b
Nick Spreitzer <nick@200-ok.com>
2019-10-03 13:14:46 -0500

    Add method to wrap long text

--- src/JournalCli/Infrastructure/Extensions.cs
+++ src/JournalCli/Infrastructure/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using JournalCli.Core;
@@ -11,5 +12,24 @@ namespace JournalCli.Infrastructure
         public static bool ContainsAll<T>(this IEnumerable<T> target, IEnumerable<T> matches) => matches.All(target.Contains);
 
         public static string ToJournalEntryFileName(this LocalDate date) => date.ToString(Journal.FileNameWithExtensionPattern.PatternText, CultureInfo.CurrentCulture);
+
+        public static string Wrap(this string text, int width = 120)
+        {
+            if (text.Length <= width)
+                return text;
+
+            const char delimiter = ' ';
+            var words = text.Split(delimiter);
+            var allLines = words.Skip(1).Aggregate(words.Take(1).ToList(), (lines, word) =>
+            {
+                if (lines.Last().Length + word.Length >= width - 1) // Minus 1, to allow for newline char
+                    lines.Add(word);
+                else
+                    lines[lines.Count - 1] += delimiter + word;
+                return lines;
+            });
+
+            return string.Join(Environment.NewLine, allLines.ToArray());
+        }
     }
 }
