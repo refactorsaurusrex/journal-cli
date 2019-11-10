231b6337d3afe5aa8e970794407edfd5d0c6a549
Nick Spreitzer <nick@200-ok.com>
2019-11-10 14:44:16 -0600

    Clarify intended entry date if created after midnight

---
+++ src/JournalCli/Infrastructure/Now.cs
@@ -0,0 +1,10 @@
+﻿using NodaTime;
+using NodaTime.Extensions;
+
+namespace JournalCli.Infrastructure
+{
+    internal static class Now
+    {
+        public static LocalTime Time() => SystemClock.Instance.InTzdbSystemDefaultZone().GetCurrentLocalDateTime().TimeOfDay;
+    }
+}
