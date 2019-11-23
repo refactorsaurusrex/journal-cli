b9a7c933ff045a12de6500958872b16aef6948ed
Nick Spreitzer <nick@200-ok.com>
2019-11-22 22:40:17 -0600

    Add cmdlet to return recent entries

---
+++ src/JournalCli/Cmdlets/GetRecentJournalEntriesCmdlet.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Abstractions;
+using System.Management.Automation;
+using JetBrains.Annotations;
+using JournalCli.Core;
+using JournalCli.Infrastructure;
+
+namespace JournalCli.Cmdlets
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Get, "RecentJournalEntries")]
+    public class GetRecentJournalEntriesCmdlet : JournalCmdletBase
+    {
+        [Parameter]
+        public int Limit { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            var fileSystem = new FileSystem();
+            var readerWriterFactory = new JournalReaderWriterFactory(fileSystem, Location);
+            var markdownFiles = new MarkdownFiles(fileSystem, Location);
+            var systemProcess = new SystemProcess();
+            var journal = Journal.Open(readerWriterFactory, markdownFiles, systemProcess);
+
+            var entries = journal.GetRecentEntries(Limit);
+            WriteObject(entries, true);
+        }
+    }
+}
