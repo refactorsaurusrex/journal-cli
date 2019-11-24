5e96fa315ad2960fd907125aed651beb833f0390
Nick Spreitzer <nick@200-ok.com>
2019-11-24 12:39:29 -0600

    Add cmdlet to load journal files
    The primary use case for this is to simplify using Select-String on all journal files.

---
+++ src/JournalCli/Cmdlets/GetJournalFilesCmdlet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Management.Automation;
+using JetBrains.Annotations;
+using JournalCli.Core;
+using JournalCli.Infrastructure;
+
+namespace JournalCli.Cmdlets
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Get, "JournalFiles")]
+    public class GetJournalFilesCmdlet : JournalCmdletBase
+    {
+        [Parameter]
+        public DateTime? From { get; set; }
+
+        [Parameter]
+        public DateTime? To { get; set; }
+
+        [Parameter]
+        public string[] Tags { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            var dateRange = GetRangeOrThrow(From, To);
+
+            if (dateRange == null && Tags == null)
+            {
+                var fileSystem = new FileSystem();
+                var markdownFiles = new MarkdownFiles(fileSystem, Location);
+                var results = markdownFiles.FindAll().Select(x => new FileInfo(x));
+                WriteObject(results, true);
+
+                return;
+            }
+
+            var journal = OpenJournal();
+            var index = journal.CreateIndex<JournalEntryFile>(dateRange, Tags);
+            var entries = index.SelectMany(x => x.Entries).Distinct().Select(x => new FileInfo(x.FilePath));
+            WriteObject(entries, true);
+        }
+    }
+}
