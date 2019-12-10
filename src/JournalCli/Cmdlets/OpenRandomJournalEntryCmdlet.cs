c65c04dc660945f567daa4fc3b8a216ecf625df3
Nick Spreitzer <nick@200-ok.com>
2019-12-09 21:33:50 -0600

    Enable opening random entries from a specific year

--- src/JournalCli/Cmdlets/OpenRandomJournalEntryCmdlet.cs
+++ src/JournalCli/Cmdlets/OpenRandomJournalEntryCmdlet.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Management.Automation;
 using JetBrains.Annotations;
+using JournalCli.Infrastructure;
+using NodaTime;
 
 namespace JournalCli.Cmdlets
 {
@@ -15,13 +17,17 @@ namespace JournalCli.Cmdlets
         [Parameter]
         public DateTime To { get; set; } = DateTime.Now;
 
+        [Parameter(ParameterSetName = "Year", Mandatory = true)]
+        public int Year { get; set; }
+
         [Parameter]
         public string[] Tags { get; set; }
 
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
-            var range = GetRangeOrNull(From, To);
+
+            var range = ParameterSetName == "Year" ? new DateRange(new LocalDate(Year, 1, 1), new LocalDate(Year, 12, 31)) : GetRangeOrNull(From, To);
             var journal = OpenJournal();
 
             journal.OpenRandomEntry(Tags, range);
