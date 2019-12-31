61daf95f6bced8af5abf2932a5308ce9bf0da85b
Nick Spreitzer <nick@200-ok.com>
2019-12-31 11:02:06 -0600

    Use splatting for PS command

--- deploy.ps1
+++ deploy.ps1
@@ -4,10 +4,13 @@ if ($env:APPVEYOR_REPO_TAG -eq 'true') {
   Publish-Module -NuGetApiKey $env:psgallery -Path .\publish\JournalCli
   Write-Host "Package successfully published to the PowerShell Gallery."
 } else {
-  Register-PSRepository -Name "MyGet" 
-    -SourceLocation 'https://www.myget.org/F/journal-cli/api/v2' 
-    -PublishLocation 'https://www.myget.org/F/journal-cli/api/v2/package'
-    -InstallationPolicy Trusted
+  $options = @{
+    Name = 'MyGet'
+    SourceLocation = 'https://www.myget.org/F/journal-cli/api/v2'
+    PublishLocation = 'https://www.myget.org/F/journal-cli/api/v2/package'
+    InstallationPolicy = 'Trusted'
+  }
+  Register-PSRepository @options
   Publish-Module -NuGetApiKey $env:myget -Path .\publish\JournalCli -Repository MyGet
   Write-Host "Package successfully published to the MyGet pre-release feed."
 }
