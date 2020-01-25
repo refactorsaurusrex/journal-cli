4675480981eda2371517b8074be174618bf98bc3
Nick Spreitzer <nick@200-ok.com>
2020-01-25 10:05:57 -0600

    Do not deploy if not master and no new tags pushed

--- deploy.ps1
+++ deploy.ps1
@@ -4,17 +4,18 @@ if ($env:APPVEYOR_REPO_TAG -eq 'true') {
   Write-Host "Publishing to the PowerShell Gallery..."
   Publish-Module -NuGetApiKey $env:psgallery -Path .\publish\JournalCli
   Write-Host "Package successfully published to the PowerShell Gallery."
-} else {
+} elseif ($env:APPVEYOR_REPO_BRANCH -eq 'master') {
   $options = @{
     Name = 'MyGet'
     SourceLocation = 'https://www.myget.org/F/journal-cli/api/v2'
     PublishLocation = 'https://www.myget.org/F/journal-cli/api/v2/package'
     InstallationPolicy = 'Trusted'
   }
-
   Write-Host "Registering the MyGet Gallery..."
   Register-PSRepository @options
   Write-Host "Publishing to the MyGet Gallery..."
   Publish-Module -NuGetApiKey $env:myget -Path .\publish\JournalCli -Repository MyGet
   Write-Host "Package successfully published to the MyGet pre-release feed."
+} else {
+  Write-Host "Either this is a non-master branch build or no tags were pushed. Skipping deployment."
 }
