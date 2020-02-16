d1e9490e2fc9bbcbc4137b57d9798fc13401028e
Nick Spreitzer <nick@200-ok.com>
2020-02-16 12:01:44 -0600

    Rename cmdlets for increased uniformity
    As much as possible, I'd like to use the format of Verb-JournalNoun. The only exceptions should be when it really just doesn't make sense, like 'Open-JournalEntryRandom' - which just sounds stupid.

--- src/JournalCli/Cmdlets/SetJournalDefaultLocationCmdlet.cs
+++ src/JournalCli/Cmdlets/SetJournalDefaultLocationCmdlet.cs
@@ -6,14 +6,18 @@ using JournalCli.Infrastructure;
 namespace JournalCli.Cmdlets
 {
     [PublicAPI]
-    [Cmdlet(VerbsCommon.Set, "DefaultJournalLocation")]
-    public class SetDefaultJournalLocationCmdlet : CmdletBase
+    [Cmdlet(VerbsCommon.Set, "JournalDefaultLocation")]
+    [Alias("Set-DefaultJournalLocation")]
+    public class SetJournalDefaultLocationCmdlet : CmdletBase
     {
         [Parameter(Mandatory = true, Position = 0)]
         public string Location { get; set; }
 
         protected override void ProcessRecord()
         {
+            if (MyInvocation.InvocationName == "Set-DefaultJournalLocation")
+                WriteWarning("'Set-DefaultJournalLocation' is obsolete and will be removed in a future release. Use 'Set-JournalDefaultLocation' instead.");
+
             var encryptedStore = EncryptedStoreFactory.Create<UserSettings>();
             var settings = UserSettings.Load(encryptedStore);
             settings.DefaultJournalRoot = Location;
