630a007cda995ccb6c3e0e7e3af912412115a86e
Nick Spreitzer <nick@200-ok.com>
2020-02-16 11:14:32 -0600

    Allow release configuration debugging

--- debug.ps1
+++ debug.ps1
@@ -1,10 +1,11 @@
 param(
-    [Parameter(Position = 0, Mandatory = $true)][string]$ProjectName
+    [Parameter(Position = 0, Mandatory = $true)][string]$ProjectName,
+    [Parameter(Position = 1)]$Configuration = "Debug"
 )
 
 $ErrorActionPreference = 'Stop'
 
 Remove-Item "$PSScriptRoot\publish\$ProjectName\" -Recurse -Force -ErrorAction SilentlyContinue
 $proj = Get-ChildItem -Filter "$ProjectName.csproj" -Recurse | Select-Object -First 1 -ExpandProperty FullName
-dotnet publish $proj -r 'win10-x64' --output "$PSScriptRoot\publish\$ProjectName" --self-contained true
+dotnet publish $proj -r 'win10-x64' --output "$PSScriptRoot\publish\$ProjectName" --self-contained true -c $Configuration
 Import-Module "$PSScriptRoot\publish\$ProjectName\$ProjectName.dll"
