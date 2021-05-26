2927d5ebe1737196e6ead806983502270c1f3230
Nick Spreitzer <nick@200-ok.com>
2021-05-26 10:54:30 -0500

    feat: Enable S3 synchronization

---
+++ src/JournalCli/Cmdlets/SyncJournalCmdlet.cs
@@ -0,0 +1,15 @@
+﻿using System.Management.Automation;
+using JetBrains.Annotations;
+
+namespace JournalCli.Cmdlets
+{
+    [PublicAPI]
+    [Cmdlet(VerbsData.Sync, "Journal")]
+    public class SyncJournalCmdlet : JournalCmdletBase
+    {
+        protected override void RunJournalCommand()
+        {
+
+        }
+    }
+}
