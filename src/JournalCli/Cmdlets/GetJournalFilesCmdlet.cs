b119e6972b3a22b6e54282e33745d4a060f7a87a
Nick Spreitzer <nick@200-ok.com>
2021-07-10 09:12:12 -0500

    feat: Enable getting journal files by entry name

--- src/JournalCli/Cmdlets/GetJournalFilesCmdlet.cs
+++ src/JournalCli/Cmdlets/GetJournalFilesCmdlet.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Management.Automation;
 using JetBrains.Annotations;
 using JournalCli.Core;
@@ -12,37 +13,48 @@ namespace JournalCli.Cmdlets
     [OutputType(typeof(PSObject[]))]
     public class GetJournalFilesCmdlet : JournalCmdletBase
     {
-        [Parameter]
+        [Parameter(ParameterSetName = "Default")]
         [NaturalDate(RoundTo.StartOfPeriod)]
         public LocalDate From { get; set; }
 
-        [Parameter]
+        [Parameter(ParameterSetName = "Default")]
         [NaturalDate(RoundTo.EndOfPeriod)]
         public LocalDate To { get; set; } = Today.Date();
 
-        [Parameter]
+        [Parameter(ParameterSetName = "Default")]
         [ArgumentCompleter(typeof(TagCompleter))]
         public string[] Tags { get; set; }
 
-        [Parameter]
+        [Parameter(ParameterSetName = "Default")]
         public TagOperator TagsOperator { get; set; } = TagOperator.Any;
 
-        [Parameter]
+        [Parameter(ParameterSetName = "Default")]
         public SortOrder Direction { get; set; } = SortOrder.Descending;
 
-        [Parameter]
+        [Parameter(ParameterSetName = "Default")]
         [WildcardInt]
         public int? Limit { get; set; } = 30;
+        
+        [Parameter(ParameterSetName = "Names", Position = 0, ValueFromPipeline = true)]
+        public string[] EntryNames { get; set; }
 
         protected override void EndProcessing()
         {
             base.EndProcessing();
-
             var journal = OpenJournal();
-            From = LocalDate.Max(From, journal.FirstEntryDate);
-            var dateRange = new DateRange(From, To);
 
-            var entries = journal.GetEntries<JournalEntryFile>(Tags, TagsOperator, Direction, dateRange, Limit).Select(x => PathToPSObject(x.FilePath));
+            IEnumerable<PSObject> entries;
+            if (ParameterSetName == "Names")
+            {
+                entries = EntryNames.Select(x => PathToPSObject(journal.GetEntryFromName(x).FilePath));
+            }
+            else
+            {
+                From = LocalDate.Max(From, journal.FirstEntryDate);
+                var dateRange = new DateRange(From, To);
+                entries = journal.GetEntries<JournalEntryFile>(Tags, TagsOperator, Direction, dateRange, Limit).Select(x => PathToPSObject(x.FilePath));
+            }
+            
             WriteObject(entries, true);
         }
 
